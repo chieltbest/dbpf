395f77d8364bdc7a5d10a928e89e28d13ffeea1f
Chiel Douwes <chieldouwes@gmail.com>
2025-04-01 05:27:51 +0200

    fix windows build

--- dbpf_utils/build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    embed_resource::compile_for("res/batl.rc", &["batl"], embed_resource::NONE).manifest_required().unwrap();
-    embed_resource::compile_for("res/yact.rc", &["yact"], embed_resource::NONE).manifest_required().unwrap();
-    embed_resource::compile_for("res/yape.rc", &["yape"], embed_resource::NONE).manifest_required().unwrap();
-}
