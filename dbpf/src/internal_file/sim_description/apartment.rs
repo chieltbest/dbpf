a50f30538a03b290dc88cc796fcc729dfe5f54d7
Chiel Douwes <chieldouwes@gmail.com>
2025-11-18 17:42:14 +0100

    refactor(dbpf): add TitlePostName enum

--- dbpf/src/internal_file/sim_description/apartment.rs
+++ dbpf/src/internal_file/sim_description/apartment.rs
@@ -3,12 +3,35 @@ use binrw::binrw;
 #[binrw]
 #[derive(Clone, Debug, Default, Eq, PartialEq)]
 pub struct ApartmentLifePreReleaseData {
-	pub reputation: u16,
+	/// -100 - 100
+	pub reputation: i16,
 	pub probability_to_appear: u16,
+}
+
+#[binrw]
+#[brw(repr = u16)]
+#[derive(Clone, Debug, Default, Eq, PartialEq)]
+pub enum TitlePostName {
+	#[default]
+	None = 0x0,
+	AtrociouslyEvilWitch = 0x1,
+	AtrociouslyEvilWarlock = 0x2,
+	EvilWitch = 0x3,
+	EvilWarlock = 0x4,
+	MeanWitch = 0x5,
+	MeanWarlock = 0x6,
+	Witch = 0x7,
+	Warlock = 0x8,
+	NiceWitch = 0x9,
+	NiceWarlock = 0xA,
+	GoodWitch = 0xB,
+	GoodWarlock = 0xC,
+	InfalliblyGoodWitch = 0xD,
+	InfalliblyGoodWarlock = 0xE,
 }
 
 #[binrw]
 #[derive(Clone, Debug, Default, Eq, PartialEq)]
 pub struct ApartmentLifeData {
-	pub title_post_name: u16,
+	pub title_post_name: TitlePostName,
 }
